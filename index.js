da65b3652922bf2ed31bf68db2d0e7cf72d3c700
MuhammadFahadgithub <159857192+MuhammadFahadgithub@users.noreply.github.com>
2024-03-19 01:27:03 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+//function
+// basic syntex
+function halffryEgg() {
+    var cooked = 1 + 1.5 + 2;
+    return cooked;
+}
+//halffryEgg() //invoking the function
+//returning function values
---
+++ index.ts
@@ -0,0 +1,19 @@
+//function
+// basic syntex
+// function halffryEgg():string{
+// let cooked:string = "HELLO" + "UBAID"
+// return cooked
+// }
+
+//halffryEgg() //invoking the function
+
+//returning function values
+
+// PARAMETERS AND ARGUMENTS
+
+Function halffryEgg(egg:number, butter:number, salt:number):
+number
+return egg + butter+ salt;
+}
+let response : number= halffryEgg(1, 1-5, 2)
+console.log(response)
